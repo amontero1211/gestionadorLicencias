511d4022342797889e53bc4b30947b8fe8e91df4
Isaias Salas <issaiassalas@gmail.com>
2025-05-28 21:09:48 -0400

    Comment out cookie-parser and morgan middleware in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
-import * as morgan from 'morgan';
+// import * as cookieParser from 'cookie-parser';
+// import * as morgan from 'morgan';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('🚀 ~ bootstrap ~ app:', app);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.use(morgan('dev'));
-  app.use(cookieParser());
+  // app.use(morgan('dev'));
+  // app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
